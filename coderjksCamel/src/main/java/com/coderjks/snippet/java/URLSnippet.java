2a778b07bd83d6da588a589c4f943497162ad062
guegit <ctbcban@gmail.com>
2019-06-28 13:25:00 +0900

    POST 방식 전송 함수 추가

--- coderjksCamel/src/main/java/com/coderjks/snippet/java/URLSnippet.java
+++ coderjksCamel/src/main/java/com/coderjks/snippet/java/URLSnippet.java
@@ -1,16 +1,56 @@
-package com.coderjks.java;
+package com.coderjks.snippet.java;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
 import java.net.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class URLTest {
+public class URLSnippet {
 
+    /**
+     * SOURCE: https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+     * @throws Exception
+     */
+    public void sednPost() throws Exception {
+        final String USER_AGENT = "Mozilla/5.0";
+        String url = "https://selfsolve.apple.com/wcResults.do";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+    }
     public void sendURLGET() {
         HttpURLConnection connection;
         BufferedReader in;
