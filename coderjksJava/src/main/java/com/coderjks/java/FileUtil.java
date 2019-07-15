6c0d42f76aef6967707f3b949e12896695652e79
guegit <ctbcban@gmail.com>
2019-07-15 18:37:09 +0900

    FileMv 코드 개선
    properties 에서 경로 가져오도록 수정, 이미 대상에 파일이 존재할 경우 삭제하도록 수정

--- coderjksJava/src/main/java/com/coderjks/java/FileUtil.java
+++ coderjksJava/src/main/java/com/coderjks/java/FileUtil.java
@@ -2,41 +2,83 @@ package com.coderjks.java;
 
 import org.apache.http.client.utils.DateUtils;
 import org.junit.Test;
+import sun.security.tools.policytool.Resources_ko;
 
-import java.io.File;
-import java.io.IOException;
+import javax.annotation.Resources;
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class FileUtil {
 
     @Test
-    public void fileMv() {
-        String origPath = "d:/1.GIT/WatchAll_ETLAgent/source/WatchAll_ETLAgent/v10/java";
+    public void readProperties() {
+        Properties properties = new Properties();
 
         try {
-            List<Path> result = Files.walk(Paths.get(origPath))
-                    .filter(Files::isRegularFile)
-                    .filter(f -> {
-                        File fi = f.toFile();
-                        Long lastModifiedTime = fi.lastModified();
-                        Calendar calendar = Calendar.getInstance();
-                        calendar.setTimeInMillis(lastModifiedTime);
+            InputStream is = FileUtil.class.getResourceAsStream("config.properties");
+            properties.load(is);
+
+            String srcFolder = properties.getProperty("src_folder");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void fileMv() {
+        String srcFolder = "";
+        Properties properties = new Properties();
+
+        try {
+            InputStream is = FileUtil.class.getResourceAsStream("config.properties");
+            properties.load(is);
+
+            srcFolder = properties.getProperty("src_folder");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-                        System.out.println((System.currentTimeMillis() - lastModifiedTime) / 1000L);
-                        return true;
-                    })
-                    .collect(Collectors.toList())
+        try {
+            List<Path> result = Files.walk(Paths.get(srcFolder))
+                .filter(Files::isRegularFile)
+                .filter(f -> {
+                    String dstFiles = properties.getProperty("dst_files");
+                    String dstFolder = properties.getProperty("dst_folder");
+                    File fi = f.toFile();
+                    if ((fi.getName()).indexOf(dstFiles) > -1 && fi.getName().indexOf(".class") > -1) {
+                        try {
+                            Path newFile = Paths.get(dstFolder).resolve(f.getFileName());
+                            if (Files.exists(newFile)) {
+                                Files.delete(newFile);
+                            }
+                            Files.copy(f, newFile);
+                            return true;
+                        } catch (FileAlreadyExistsException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                        return false;
+                })
+                .collect(Collectors.toList())
 //                    .forEach(System.out::println)
-                    ;
+                ;
 
-            System.out.println(result.get(0));
+            System.out.println(result.toString());
 //            System.out.println(result);
         } catch (IOException e) {
             e.printStackTrace();
